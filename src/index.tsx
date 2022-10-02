ec44f4e824e4094a0d738797725852e789781a2d
shlev9d <shlev9d@gmail.com>
2022-10-02 17:46:56 +0300

    add redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import './styles/index.scss'
 import App from './App'
-import { store } from './store/store'
+import store, { persistor } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
 				<App />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from 'redux-persist/lib/storage'
+import {
+    persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist'
 import citySlice from "./citySlice/citySlice";
 import otherDaysSlice from "./otherDaysSlice/otherDaysSlice";
 import themeSlice from "./themeSlice/themeSlice";
 import thisDaySlice from "./thisDaySlice/thisDaySlice";
 
+const rootReducer = combineReducers({
+	theme: themeSlice,
+	thisDay: thisDaySlice,
+	otherDays: otherDaysSlice,
+	city: citySlice,
+})
 
+const persistConfig = {
+	key: 'root',
+	storage,
+}
 
-export const store = configureStore({
-	reducer: {
-    theme: themeSlice,
-    thisDay: thisDaySlice,
-    otherDays: otherDaysSlice,
-    city: citySlice
-	},
+const persistedReducer = persistReducer(persistConfig,rootReducer)
 
+const store = configureStore({
+	reducer: persistedReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
 })
 
 
+export default store
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
